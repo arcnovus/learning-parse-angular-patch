b41a6f04757085b3be2bb9d553c84f2576299c1b
onearc <appdev@onearc.com>
2014-03-25 08:41:06 -0400

    first commit

---
+++ controllers/home.js
@@ -0,0 +1,12 @@
+(function (window, angular, Parse) {
+    'use strict';
+    var HomeController;
+
+    HomeController = function ($scope, $log) {
+
+
+    };
+
+    angular.module('MainApp').controller('Home', HomeController);
+
+}(this, this.angular, this.Parse));
